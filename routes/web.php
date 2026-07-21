550716e1b2a6bad869d024a0143fd6b57b62ac8b
Ihsane-12 <benminaihssane@gmail.com>
2026-07-21 12:18:18 +0100

    creation des Routes pour Authentification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register',[AuthController::class,'showRegister']);
+Route::post('/register',[AuthController::class,'register']);
+
+Route::get('/login',[AuthController::class,'showlogin'])->name('login');
+Route::post('/login',[AuthController::class,'login']);
+
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
