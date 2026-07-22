dee8b60b19ed0a1aef0bd1e54d90b5ad36697427
Ihsane-12 <benminaihssane@gmail.com>
2026-07-22 14:14:29 +0100

    mise en place l'interface utilisateur pour l'authentification

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,10 @@ Route::post('/login',[AuthController::class,'login'])->name('login.submit');
 
 Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
 
+Route::get('/student/dashboard', function () {
+    return view('student.dashboard');
+})->name('student.dashboard');
