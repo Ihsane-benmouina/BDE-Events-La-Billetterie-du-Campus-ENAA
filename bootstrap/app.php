683698038cf1763afddf34cdfabc3e482450c300
Ihsane-12 <benminaihssane@gmail.com>
2026-07-22 14:41:12 +0100

    faire la securisation des routes avec midlweares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+   use App\Http\Middleware\IsAdmin;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,9 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+
+->withMiddleware(function ($middleware) {
+
+    $middleware->alias([
+        'isAdmin' => IsAdmin::class,
+    ]);
+
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,30 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
+Route::middleware('guest')->group(function () {
 
+    Route::get('/login', [AuthController::class, 'showLogin'])
+        ->name('login');
 
-Route::get('/login',[AuthController::class,'showLogin'])->name('login');
-Route::post('/login',[AuthController::class,'login'])->name('login.submit');
+    Route::post('/login', [AuthController::class, 'login'])
+        ->name('login.submit');
+});
 
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
-})->name('admin.dashboard');
+})
+->middleware(['auth', 'isAdmin'])
+->name('admin.dashboard');
 
 Route::get('/student/dashboard', function () {
     return view('student.dashboard');
-})->name('student.dashboard');
+})
+->middleware('auth')
+->name('student.dashboard');
